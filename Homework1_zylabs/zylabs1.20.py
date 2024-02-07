b8239e49aeae6bca458c8889b7db2718e4dcb6aa
Maimuna Murad <157168971+Maimunamurad@users.noreply.github.com>
2024-02-07 14:22:24 -0600

    Add files via upload

---
+++ Homework1_zylabs/zylabs1.20.py
@@ -0,0 +1,14 @@
+#Maimuna Murad
+#2065973
+
+user_num1 = int(input("Enter integer:\n"))
+
+print("You entered:", user_num1)
+
+print(f"{user_num1} squared is {user_num1 ** 2}")
+print(f"And {user_num1} cubed is {user_num1 ** 3} !!")
+
+user_num2 = int(input("Enter another integer:\n"))
+
+print(f"{user_num1} + {user_num2} is {user_num1 + user_num2}")
+print(f"{user_num1} * {user_num2} is {user_num1 * user_num2}")
