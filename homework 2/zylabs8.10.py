bc64215ae5763a92b1c1e8ef76e03bdb5c593541
Maimuna Murad <157168971+Maimunamurad@users.noreply.github.com>
2024-02-28 21:33:41 -0600

    Add files via upload

---
+++ homework 2/zylabs8.10.py
@@ -0,0 +1,15 @@
+def is_palindrome(input_str):
+    # Remove spaces from the input string
+    input_str = input_str.replace(" ", "")
+
+    # Check if the string is equal to its reverse
+    return input_str == input_str[::-1]
+
+
+if __name__ == '__main__':
+    input_word = input()
+
+    if is_palindrome(input_word.lower()):  # Convert to lowercase for case-insensitive comparison
+        print(f"{input_word} is a palindrome")
+    else:
+        print(f"{input_word} is not a palindrome")
